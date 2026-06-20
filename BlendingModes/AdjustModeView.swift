912bd65515caf2de4a5bf47b90b04bc403a80088
Bryan <alelinapps@icloud.com>
2026-06-20 13:26:31 -0400

    Removed dead code

--- BlendingModes/AdjustModeView.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-
-// AdjustModeView is now folded into LayerRowView directly.
-// This file is kept as a stub so any remaining references compile.
-struct AdjustModeView: View {
-    @Binding var layer: Layer
-    var body: some View { EmptyView() }
-}
--- BlendingModes/BlendModeDetail.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-// Functionality merged into ContentView
-struct BlendModeDetail: View {
-    let mode: BlendMode
-    var body: some View { EmptyView() }
-}
